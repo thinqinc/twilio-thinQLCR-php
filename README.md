e5d3af2c247133359a98f5cb5dfbb8b787bedbd6
Chris Hoskins <chrishoskins@choskins.local>
2016-03-14 11:19:29 -0400

    first version of thinQLCR-php library

---
+++ README.md
@@ -0,0 +1,5 @@
+# twilio-thinQLCR-php, Twilio Wrapper PHP Library For thinQ LCR integration
+
+##**Note that you will need a valid LCR Account with thinQ before using the libraries. For more information please contact your thinQ Sales representative at [http://www.thinq.com/library/](http://www.thinq.com/library/)**
+
+#### *Copyright (c) 2016 thinQ*
---
+++ src/TwilioWithThinQLCR/TwilioWrapper.php
@@ -0,0 +1,41 @@
+<?php
+namespace TwilioWithThinQLCR;
+//require_once __DIR__ . '/../../vendor/autoload.php'; // Loads the library
+use Services_Twilio;
+
+class TwilioWrapper {
+    private $client;
+    private $twilio_account_sid;
+    private $twilio_account_token;
+    private $thinQ_id;
+    private $thinQ_token;
+    const THINQ_DOMAIN = "wap.thinq.com";
+    const TWIML_RESOURCE_URL = "http://demo.twilio.com/docs/voice.xml";
+
+    function __construct($twilio_account_sid, $twilio_account_token, $thinQ_id, $thinQ_token){
+        $this->twilio_account_sid = $twilio_account_sid;
+        $this->twilio_account_token = $twilio_account_token;
+        $this->thinQ_id = $thinQ_id;
+        $this->thinQ_token = $thinQ_token;
+
+        $this->client = new Services_Twilio($twilio_account_sid, $twilio_account_token);
+    }
+
+    public function isClientValid(){
+        return $this->client != null && $this->client->account != null;
+    }
+
+    public function call($from, $to)
+    {
+        if(!$this->isClientValid()) {
+            return "Invalid Twilio Account details.";
+        }
+
+        try{
+            $call = $this->client->account->calls->create($from, "sip:" . $to . "@". self::THINQ_DOMAIN . '?thinQid='.$this->thinQ_id . '&thinQtoken='.$this->thinQ_token, self::TWIML_RESOURCE_URL);
+            return $call->sid;
+        }catch(Exception $e){
+            return $e->getMessage();
+        }
+    }
+}
